56a9fb91332850b7f347b7f85bd4dc3a193144c4
paljor129 <simplonco@debian>
2017-02-21 12:40:57 +0100

    nodejs

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var qrcode = require("qrcode-ternimal");
 
 qrcode.generate("This will be small QRCode, eh!", {small: true},functioin(qrcode){
-  console.log(qrcode);  
+  console.log(qrcode);
 });
---
+++ main.js
@@ -0,0 +1,1 @@
+var QRCode = require("qrcode.js");
